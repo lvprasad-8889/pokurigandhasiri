b64aa75266bb0114ba90072d805e43bed6f31e9b
lvprasad-8889 <lvprasad8889@gmail.com>
2024-04-20 22:33:24 +0530

    vercel second try

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const userApis = require("./apis/userApis/userApis");
 const adminApis = require("./apis/adminApis/adminApis");
 
 
-app.get('', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 
